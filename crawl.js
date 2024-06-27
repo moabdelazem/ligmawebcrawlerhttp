29945a5e4142e54179830b644d9fce5b3710cff5
Mohamed Abdelazem <mabdelazemhahmed@gmail.com>
2024-06-27 22:15:24 +0300

    crawl single page and error handling and adding prettier rc file

--- crawl.js
+++ crawl.js
@@ -1,42 +1,65 @@
 const { JSDOM } = require("jsdom");
 
-function getUrlsFromHTML(htmlBody, baseUrl) {
-  const urls = [];
-  const linksDOM = new JSDOM(htmlBody);
-
-  const linkElements = linksDOM.window.document.querySelectorAll("a");
-  for (let linkElement of linkElements) {
-    if (linkElement.href.slice(0, 1) === "/") {
-      try {
-        const objUrl = new URL(`${baseUrl}${linkElement.href}`);
-        urls.push(objUrl.href);
-      } catch (error) {
-        console.log(
-          `there is something wrong with the provided url : ${error}`
-        );
-      }
-    } else {
-      try {
-        const objUrl = new URL(linkElement.href);
-        urls.push(objUrl.href);
-      } catch (error) {
-        console.log(
-          `there is something wrong with the provided url : ${error}`
-        );
-      }
+async function crawlPage(currentUrl) {
+    console.log(`actively crawling : ${currentUrl}`);
+    try {
+        const response = await fetch(currentUrl);
+        if (response.status > 399) {
+            console.log(
+                `error in fetching data ${response.status} on page ${currentUrl}`
+            );
+            return;
+        }
+        const contentType = response.headers.get("content-type");
+        if (!contentType.includes("text/html")) {
+            console.log(
+                `error non html respone, content type : ${contentType} on page ${currentUrl}`
+            );
+            return;
+        }
+        console.log(await response.text());
+    } catch (error) {
+        console.log(`error in fetch : ${error}`);
     }
-  }
-  return urls;
+}
+
+function getUrlsFromHTML(htmlBody, baseUrl) {
+    const urls = [];
+    const linksDOM = new JSDOM(htmlBody);
+
+    const linkElements = linksDOM.window.document.querySelectorAll("a");
+    for (let linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === "/") {
+            try {
+                const objUrl = new URL(`${baseUrl}${linkElement.href}`);
+                urls.push(objUrl.href);
+            } catch (error) {
+                console.log(
+                    `there is something wrong with the provided url : ${error}`
+                );
+            }
+        } else {
+            try {
+                const objUrl = new URL(linkElement.href);
+                urls.push(objUrl.href);
+            } catch (error) {
+                console.log(
+                    `there is something wrong with the provided url : ${error}`
+                );
+            }
+        }
+    }
+    return urls;
 }
 
 // Normalize The Url From The User
 function normalizeUrl(urlString) {
-  const objUrl = new URL(urlString);
-  const hostPath = `${objUrl.hostname}${objUrl.pathname}`;
-  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
-    return hostPath.slice(0, -1);
-  }
-  return hostPath;
+    const objUrl = new URL(urlString);
+    const hostPath = `${objUrl.hostname}${objUrl.pathname}`;
+    if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+        return hostPath.slice(0, -1);
+    }
+    return hostPath;
 }
 
-module.exports = { normalizeUrl, getUrlsFromHTML };
+module.exports = { normalizeUrl, getUrlsFromHTML, crawlPage };
--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Hello, World");
+const { crawlPage } = require("./crawl.js");
+
+function main() {
+    if (process.argv.length < 3) {
+        console.log("no website provided!");
+        process.exit(1);
+    }
+    if (process.argv.length > 3) {
+        console.log("too may command line args provided!");
+        process.exit(1);
+    }
+    const baseUrl = process.argv[2];
+    console.log(`starting crawling on ${baseUrl}...`);
+    crawlPage(baseUrl);
+}
+
+main();
